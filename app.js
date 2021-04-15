1e6ddc204b1a1f84e2b06f7c133ed080b489b351
lorenzo torlontano <lorenzo.torlontano@gmail.com>
2021-04-15 14:48:24 +0200

    second commit

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  'mongo "mongodb+srv://node.f16ue.mongodb.net/node" --username lorenzo'
+);
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// router switch should handle requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use("/products", productRoutes);
 app.use("/orders", ordersRoutes);
 
